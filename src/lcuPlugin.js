308e5bf156838cf1a880d2d243831e3d7a746722
Philip Ross <dev@baseballlover723.com>
2023-11-20 20:55:00 -0800

    added lcu plugin file

---
+++ src/lcuPlugin.js
@@ -0,0 +1,43 @@
+export default class LCUPlugin {
+  constructor() {
+    if (new.target === LCUPlugin) {
+      throw new TypeError("Abstract class LCUPlugin can't be instantiated.");
+    }
+    this.eventSubscriptions = {};
+  }
+
+  /**
+   * You should override this function to do any setup and
+   * subscribe to any events you want to listen to.
+   * If you return a promise (use `createPromise`),
+   * then it will wait until the promise resolves before subscribing to events.
+   * @param clientData
+   */
+  onConnect(clientData) {
+  }
+
+  /**
+   * You should override this function to do any clean up when the league client is closed.
+   * If you return a promise (use `createPromise`),
+   * then it will wait until the promise resolves before subscribing to events.
+   */
+  onClose() {
+  }
+
+  /**
+   * Call this function to do any async stuff in onConnect or onClose.
+   * @param handler
+   * @returns {Promise<unknown>}
+   */
+  createPromise(handler) {
+    return new Promise(handler.bind(this));
+  }
+
+  subscribeEvent(event, handler) {
+    this.eventSubscriptions[event] = handler;
+  }
+
+  unsubscribeEvent(event) {
+    delete this.eventSubscriptions[event];
+  }
+}
